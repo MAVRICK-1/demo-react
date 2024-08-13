eed9e6371c009c581ed4fd458c004aa86d49673e
Rishi Mondal <mavrickrishi@gmail.com>
2024-08-14 01:02:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import video from './deadpool-wolverine-bye-bye.mp4'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src={video}
+        autoPlay
+        loop
+        controls
+        preload="auto"
+      />
     </div>
   );
 }
